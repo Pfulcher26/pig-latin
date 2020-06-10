d5a4240ba60def8013c48e06643f41b56f0be999
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-10 11:35:54 -0700

    verify that user input form recieves and displays data correctly.
    Co-authored-by: Payne Fulcher <pfulcher26@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+
 $(document).ready(function() {
-  
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    
+    $("#output").text(number1);
+  });
 });
