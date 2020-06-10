71c0a2e591e8dce0196ba77b76f2d4d8c18d91dd
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-10 11:45:54 -0700

    changed input to accept string's instead of numerical values
    Co-authored-by: Payne Fulcher <pfulcher26@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-
 $(document).ready(function() {
+
   $("form#calculator").submit(function() {
     event.preventDefault();
-    const number1 = parseInt($("#input1").val());
-    
+    const number1 = $("#input1").val();
+
     $("#output").text(number1);
   });
 });
