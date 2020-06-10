c3f5eb448703eab7c88764a74f2f0a907d382346
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-10 14:18:01 -0700

    create fuctioning first apenddix rule for user input
    Co-authored-by: Payne Fulcher <pfulcher26@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,34 @@
-$(document).ready(function() {
+function translatePigLatin(str) {
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  let newStr = "";
 
+  if (vowels.indexOf(str[0]) > -1) {
+      newStr = str + "way";
+      return newStr;
+  } else {
+      let firstMatch = str.match(/[aeiou]/g) || 0;
+      let vowel = str.indexOf(firstMatch[0]);
+      newStr = str.substring(vowel) + str.substring(0, vowel) + "ay";
+      return newStr;
+  }
+}
+
+$(document).ready(function() {
   $("form#calculator").submit(function() {
     event.preventDefault();
-    const number1 = $("#input1").val();
+    let number1 = $("#input1").val();
+    let result = translatePigLatin(number1);
+   
 
-    $("#output").text(number1);
+    $("#output").text(result);
   });
 });
+
+
+//let arrayOfVowels = ["a", "e", "i", "o", "u"]
+
+//if number1[0]  === arrayOfVowels {    
+    
+//}
+//}
+
